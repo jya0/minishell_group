b3227bd64729c2e252e8a6608e81a1f2b357fdba
Jinxiu Yao <jyao@lab2r2s1.42abudhabi.ae>
2022-12-11 16:32:19 +0400

    updated tokenizer functions and started main parser

--- includes/sh_parser.h
+++ includes/sh_parser.h
@@ -6,7 +6,7 @@
 /*   By: jyao <jyao@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 14:08:26 by jyao              #+#    #+#             */
-/*   Updated: 2022/12/11 12:43:28 by jyao             ###   ########.fr       */
+/*   Updated: 2022/12/11 16:30:06 by jyao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,43 +17,36 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-/*==================defining constants======================*/
+/*==================DEFINING CONSTANTS======================*/
 
 # define TOK_DELIM	" \n\t\v\f\r"
 
-/*==================defining constants======================*/
+/*==================SECTION FOR TOKENIZING===============*/
 
-
+typedef struct s_token {
+	int		str_len;
+	char	*str;
+}	t_token;
 
 /*=======SECTION FOR ABSTRACT SYNTAX TREE==============*/
 
-/*the members of this enum matches with 
-**the members of the struct in the same order!*/
-typedef enum e_data_type {
-	DT_NULL = -1,
-	DT_LONG_S,
-	DT_LONG_U,
-	DT_STR,
-	DT_STR_ARR
-}	t_data_type;
-
-typedef union u_data {
-	ssize_t			long_s;
-	size_t			long_u;
-	char			*str;
-	char			**str_arr;
-}	t_data;
-
+/*This is resposible to see if the t_data of the node 
+**needs further expansion or not*/
 typedef enum e_node_type {
 	NT_NULL = -1,
 	NT_TERM,
 	NT_NON_TERM
 }	t_node_type;
 
+/*Once the ast t_node_type of the node is marked as NT_TERM
+**it should be next to mark the type of the terminal in t_term_type*/
 typedef enum e_term_type {
 	TT_NULL = -1,
 	TT_CMD,
 	TT_CMD_ARGS,
+	TT_QUOTE_S,
+	TT_QUOTE_D,
+	TT_DOLLAR,
 	TT_PIPE,
 	TT_REDIR_L,
 	TT_REDIR_R,
@@ -61,6 +54,29 @@ typedef enum e_term_type {
 	TT_APPND_R
 }	t_term_type;
 
+/*the members of this enum helps to chose the members of the struct t_data!
+**Add more members as fit!*/
+typedef enum e_data_type {
+	DT_NULL = -1,
+	DT_CHAR,
+	DT_LONG_S,
+	DT_LONG_U,
+	DT_STR,
+	DT_STR_ARR,
+	DT_LST_LIBFT
+}	t_data_type;
+
+/*t_data should be expanded from the tokens got from the tokenizing functions
+**Data depends on t_term_type*/
+typedef union u_data {
+	char			c;
+	ssize_t			long_s;
+	size_t			long_u;
+	void			*ptr_w_1d;
+	void			**ptr_w_2d;
+}	t_data;
+
+/*the building block of an abstract syntax tree!*/
 typedef struct s_ast_node {
 	t_node_type			node_type;
 	t_term_type			term_type;
@@ -70,7 +86,5 @@ typedef struct s_ast_node {
 	struct s_ast_node	*child_l;
 	struct s_ast_node	*child_r;
 }	t_ast_node;
-
-/*=======SECTION FOR ABSTRACT SYNTAX TREE==============*/
 
 #endif
--- srcs/parser/sh_ps_token.c
+++ srcs/parser/sh_ps_token.c
@@ -6,7 +6,7 @@
 /*   By: jyao <jyao@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 14:37:03 by jyao              #+#    #+#             */
-/*   Updated: 2022/12/11 12:48:37 by jyao             ###   ########.fr       */
+/*   Updated: 2022/12/11 16:16:40 by jyao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,24 @@
 **because ft_split only takes one delim char and moreover this strtok
 implementation is in my opinion cleaner*/
 
-static char	*get_next_tok(const char **buf_stored, const char *delim)
+/*frees mem allocated members and the token itself*/
+void	sh_ps_token_free(t_token *token)
+{
+	if (token == NULL)
+		return ;
+	if (token->str == NULL)
+		return ;
+	free(token->str);
+	free(token);
+}
+
+/*static function to simplify sh_ps_token*/
+static t_token	*get_next_tok(const char **buf_stored, const char *delim)
 {
 	const char			*buf_tmp;
 	unsigned int		start;
 	unsigned int		end;
+	t_token				*token;
 
 	buf_tmp = *buf_stored;
 	start = 0;
@@ -35,43 +48,51 @@ static char	*get_next_tok(const char **buf_stored, const char *delim)
 		*buf_stored = NULL;
 	else
 		*buf_stored = &buf_tmp[end];
-	return (ft_substr(buf_tmp, start, end - start));
+	token = (t_token *)ft_calloc(1, sizeof(t_token));
+	if (token == NULL)
+		return (NULL);
+	token->str_len = end - start;
+	token->str = ft_substr(buf_tmp, start, token->str_len);
+	return (token);
 }
 
-/*Gets the input from readline, then returns a token string each time it is called!*/
-char	*sh_ps_token(const char *buf_src, const char *delim)
+/*Gets the input from readline, then returns a token each time it is called!*/
+t_token	*sh_ps_token(const char *buf_src, const char *delim)
 {
 	static const char	*buf_stored;
-	char				*tok;
+	t_token				*token;
 
 	if (delim == NULL || (buf_src == NULL && buf_stored == NULL))
 		return (NULL);
 	else if (buf_stored == NULL && buf_src != NULL)
 		buf_stored = buf_src;
-	tok = get_next_tok(&buf_stored, delim);
-	// if (buf_stored)
-	// 	printf("char = %c\n", *buf_stored);
-	return (tok);
+	token = get_next_tok(&buf_stored, delim);
+	return (token);
 }
 
-
 /*================Already tested for memory leaks!==================*/
+/*gcc -Wall -Wextra -Werror sh_ps_token.c -L../../libft -lft*/
 // /*
 int	main(int argc, char	*argv[])
 {
-	char	*tok;
+	t_token	*tok;
 
 	if (argc == 1)
 		return (0);
 	tok = sh_ps_token(argv[1], TOK_DELIM);
 	while (tok != NULL)
 	{
-		printf("%s %d\n", tok, ft_strlen(tok));
-		free(tok);
-		// sleep(5);
+		printf("%s$ %d\n", tok->str, tok->str_len);
+		sh_ps_token_free(tok);
 		tok = sh_ps_token(NULL, TOK_DELIM);
 	}
+	tok = sh_ps_token(NULL, TOK_DELIM);
+	printf("===NULL TEST==\n");
+	if (tok == NULL)
+		printf("NULL\n");
+	tok = sh_ps_token(NULL, TOK_DELIM);
+	if (tok == NULL)
+		printf("NULL\n");
 	return (0);
 }
 // */
-
